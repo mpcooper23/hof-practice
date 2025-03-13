805e2a0d5243c6287ccb1d7131e07695da4f5d94
Alex Aaron <alexfrancisaaron@gmail.com>
2025-03-13 11:28:48 -0500

    added letterboxd entries array

---
+++ index.js
@@ -0,0 +1,82 @@
+const lbDiary = [
+  {
+    title: 'The Rule of Jenny Pen',
+    director: 'James Ashcroft',
+    year: 2025,
+    genres: ['Drama', 'Horror', 'Thriller'],
+    runtime: {
+      unit: 'Minutes',
+      length: 104
+    },
+    stars: 3.5,
+    entryLogged: 'March 11, 2025',
+    review: null,
+    comments: [
+      {
+        username: 'elizabeth-dane',
+        text: 'Loved it!',
+        date: 'March 12, 2025'
+      },
+      {
+        username: 'rj-macready',
+        text: 'Rush and Lithgow killed it.',
+        date: 'March 12, 2025'
+      }
+    ]
+  },
+  {
+    title: 'Mickey 17',
+    director: 'Bong Joon Ho',
+    year: 2025,
+    genres: ['Comedy', 'Adventure', 'Science Fiction'],
+    runtime: {
+      unit: 'Minutes',
+      length: 137
+    },
+    stars: 3.5,
+    entryLogged: 'March 9, 2025',
+    review: "Feels like it's missing an act.",
+    comments: [
+      {
+        username: 'elizabeth-dane',
+        text: 'NO NOT THE SAUCEEEEEEEEE!!!!!',
+        date: 'March 10, 2025'
+      },
+    ]
+  },
+  {
+    title: 'The Monkey',
+    director: 'Osgood Perkins',
+    year: 2025,
+    genres: ['Comedy', 'Horror'],
+    runtime: {
+      unit: 'Minutes',
+      length: 98
+    },
+    stars: 2,
+    entryLogged: 'February 22, 2025',
+    review: "Despite the grotesquery, it feels oddly toothless and lacking stakes.",
+    comments: []
+  },
+  {
+    title: 'Okja',
+    director: 'Bong Joon Ho',
+    year: 2017,
+    genres: ['Drama', 'Science Fiction', 'Adventure'],
+    runtime: {
+      unit: 'Minutes',
+      length: 122
+    },
+    stars: 4.5,
+    entryLogged: 'January 12, 2025',
+    review: null,
+    comments: [
+      {
+        username: 'rj-macready',
+        text: 'That moment with the baby superpig at the end gets me every time.',
+        date: 'January 13, 2025'
+      }
+    ]
+  }
+];
+
